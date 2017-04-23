8b98b5cd4186fdcf2912ef4947d0b794d86ca103
Luke Brown <luke4@g.clemson.edu>
2017-04-23 19:35:09 -0400

    documentation
    added comments and a readme

---
+++ number-gussing/README.md
@@ -0,0 +1,5 @@
+My terrible number guessing cli game that I made in 30 minutes.
+
+Compile with:
+
+`g++ main.cpp -o guess.out`
--- number-gussing/main.cpp
+++ number-gussing/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
-#include <cstdlib>
+#include <cstdlib>//needed for the random number generator
 
+//declare namespaces
 using namespace std;
 
+//protptypes
 int isEqual(int, int);
 void findGuess(int, int);
 
@@ -13,11 +15,13 @@ int main(){
         //generate random seed
         srand (time(NULL));
 
+        //generate random number between 1 and 1000
         randomNum = rand() % 1000 + 1;
 
         cout << "Welcome to my number guessing game.\nPlease enter a number between 1-1000\n> ";
         cin >> guess;
 
+        //main loop
         while(isEqual(guess, randomNum)) {
                 findGuess(guess, randomNum);
                 cout << "> ";
@@ -29,12 +33,27 @@ int main(){
         return 0;
 }
 
+/**
+ * [isEqual checks if input is equal to the ran number]
+ * @param  in  [user input]
+ * @param  ran [the generated random nummber]
+ * @return     [true if equal, false if not]
+ */
 int isEqual(int in, int ran){
         if(in == ran) { return 0; }
         else { return 1; }
 }
 
+/**
+ * [findGuess determines if user input is too high or low compared to random]
+ * @param  in  [user input]
+ * @param  ran [the generated random nummber]
+ */
 void findGuess(int in, int ran){
+
+        //if diff is positive, guess was too low,
+        //if diff is negative, guesss was too high.
+        //include cases for assignment specific feedback.
         int diff = ran - in;
 
         if(diff > 0) {
